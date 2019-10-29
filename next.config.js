b37d4007ef462e411e0d4ab7f5ad10ccc7cbd579
Michael Utz <michael@theutz.com>
2019-10-29 13:47:53 +0200

    feat: add mdx to next loaders

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 const withPlugins = require('next-compose-plugins')
 const optimizedImages = require('next-optimized-images')
+const withMdx = require('@next/mdx')
 
 module.exports = withPlugins(
-  [[optimizedImages, { optimizeImagesInDev: true }]],
+  [
+    [optimizedImages, { optimizeImagesInDev: true }],
+    [
+      withMdx({ extension: /\.mdx$/ }),
+      { pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'] },
+    ],
+  ],
   {
     webpack: (config) => {
       // Fixes npm packages that depend on `fs` module
