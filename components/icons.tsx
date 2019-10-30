f2db223784a953076e641b4fe52d370532cc1c0b
Michael Utz <michael@theutz.com>
2019-10-30 14:14:31 +0200

    refactor: rename icons

--- components/icons.tsx
+++ components/icons.tsx
@@ -2,9 +2,11 @@ import {
   faUsersCrown,
   faPhoneLaptop,
   faStore,
+  faChevronDoubleDown,
 } from '@fortawesome/pro-duotone-svg-icons'
 import { makeIcon } from '../utils/icons'
 
-export const FaUsersCrown = makeIcon(faUsersCrown)
-export const FaPhoneLaptop = makeIcon(faPhoneLaptop)
-export const FaStore = makeIcon(faStore)
+export const FADUsersCrown = makeIcon(faUsersCrown)
+export const FADPhoneLaptop = makeIcon(faPhoneLaptop)
+export const FADStore = makeIcon(faStore)
+export const FADChevronDoubleDown = makeIcon(faChevronDoubleDown)
